1483f6f685dfa9019c486a5365482e31dab7dff2
Ankit <ankitkr5609@gmail.com>
2025-12-09 15:55:47 +0530

    controller early code

--- src/main/java/com/cybernode/taskmanager/controller/TaskController.java
+++ src/main/java/com/cybernode/taskmanager/controller/TaskController.java
@@ -1,4 +1,31 @@
 package com.cybernode.taskmanager.controller;
 
+import com.cybernode.taskmanager.model.Task;
+import com.cybernode.taskmanager.service.TaskService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/task")
 public class TaskController {
+
+    private TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task){
+        return taskService.createtask(task);
+    }
+
+    @GetMapping("/{id}")
+    public Task getTask(@PathVariable String id){
+        return taskService.getTask(id);
+    }
+
+    @PutMapping("/{id}")
+    public  Task updateTask(@PathVariable String id, @RequestBody Task task){
+
+    }
 }
